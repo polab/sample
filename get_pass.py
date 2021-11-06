2d4630bf60addbe8323aeeac32153d427cd9ebc2
Peter Olabowale <peter.olabowale@gmail.com>
2021-11-06 12:10:57 -0400

    Adding code to get password

---
+++ get_pass.py
@@ -0,0 +1,7 @@
+first_name = input("Please enter your name:").title()
+password = "W@12"
+
+if first_name == "Peter":
+  print("\n", f'Hello, {first_name}! The password is : {password}',"\n")
+else:
+  print(f'Hello, {first_name}! See you later.')
