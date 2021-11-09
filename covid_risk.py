a4a43327e5252f84fc8742e853d8df36f1cadb35
Peter Olabowale <peter.olabowale@gmail.com>
2021-11-09 11:18:18 -0500

    modificationn

--- covid_risk.py
+++ covid_risk.py
@@ -1,22 +1,9 @@
-
-age = False 
-chronic = False
-immune = False
-
-#age =  input("Are you a cigarette addict older than 75 years old? ")
-#chronic = input("Do you have a severe chronic disease? ")
-#immune =  input("Is your immune system too weak? ")
-
-age = bool(age)
-chronic = bool(chronic)
-immune = bool(immune)
+age =  input("Are you a cigarette addict older than 75 years old? ")
+chronic = input("Do you have a severe chronic disease? ")
+immune =  input("Is your immune system too weak? ")
 
 
-#print(type(age))
-#print(type(chronic))
-#print(type(immune))
-
-if (age == True or chronic == True or immune == True):
-  print("\n", "Covid risk is True")
-elif (age == False and chronic == False and immune == False):
-  print("Covid risk is False")
+if (age == "True" or chronic == "True" or immune == "True"):
+  print("\n", "Covid risk is True", "\n")
+elif (age == "False" and chronic == "False" and immune == "False"):
+  print("\n", "Covid risk is False", "\n")
